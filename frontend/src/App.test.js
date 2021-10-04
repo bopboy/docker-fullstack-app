bdc10a476657eaf3069cd528cc73688dda544233
bopboy2 <scp0000@gmail.com>
2021-10-05 00:40:02 +0900

    AWS 설정 완료 및 배포

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,5 +6,5 @@ test('renders learn react link', () => {
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
-  // 그냥 추가
+  // 
 });
